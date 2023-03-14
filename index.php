713424b9e77fb1c5165c1435c22d021dff9b1e1f
Dima163163 <khlopovdima@gmail.com>
2023-03-14 18:35:55 +0400

    add: footer

--- index.php
+++ index.php
@@ -280,7 +280,7 @@
                   </li>
                   <li class="card-bottom-item">
                     <svg class="card-icon" width="18" height="18">
-                      <use href="img/sprite.svg#energy"></use>
+                      <use href="img/sprite.svg#energy-grey"></use>
                     </svg>
                     1,2 л.с.
                   </li>
@@ -343,7 +343,7 @@
                   </li>
                   <li class="card-bottom-item">
                     <svg class="card-icon" width="18" height="18">
-                      <use href="img/sprite.svg#energy"></use>
+                      <use href="img/sprite.svg#energy-grey"></use>
                     </svg>
                     1,2 л.с.
                   </li>
@@ -406,7 +406,7 @@
                   </li>
                   <li class="card-bottom-item">
                     <svg class="card-icon" width="18" height="18">
-                      <use href="img/sprite.svg#energy"></use>
+                      <use href="img/sprite.svg#energy-grey"></use>
                     </svg>
                     1,2 л.с.
                   </li>
@@ -469,7 +469,7 @@
                   </li>
                   <li class="card-bottom-item">
                     <svg class="card-icon" width="18" height="18">
-                      <use href="img/sprite.svg#energy"></use>
+                      <use href="img/sprite.svg#energy-grey"></use>
                     </svg>
                     1,2 л.с.
                   </li>
@@ -556,7 +556,204 @@
         </div>
       </div>
     </div>
-    <div class="container-image footer-bottom"></div>
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="footer-bottom-inner">
+          <div class="footer-menu-wrapper">
+              <h2 class="footer-menu-title-bottom">
+                Каталог товаров
+              </h2>
+              <ul class="footer-menu-list">
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Электросамокаты</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Электроскутеры</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Электровелосипеды</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Электровелосипеды</a>
+                </li>
+              </ul>
+          </div>
+          <div class="footer-menu-wrapper">
+              <h2 class="footer-menu-title-bottom">
+                Покупателям
+              </h2>
+              <ul class="footer-menu-list footer-menu-column-2">
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Сервисный центр</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Доставка и оплата</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Рассрочка</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Тест-драйв</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Блог</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Сотрудничество</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Контакты</a>
+                </li>
+                <li class="footer-menu-item">
+                  <a href="#" class="footer-menu-link">Акции</a>
+                </li>
+              </ul>
+          </div>
+          <div class="footer-info-wrapper">
+            <div class="footer-info-top">
+              <h3 class="title footer-info-title">Контакты</h3>
+              <a href="#" class="footer-info-link">Заказать звонок</a>
+            </div>
+              <div class="footer-info-inner">
+                <div class="footer-info">
+                  <p class="footer-text">Call-центр</p>
+                  <a href="Tel:+78005055461" class="footer-phone">+7 (800) 505-54-61</a>
+                  <p class="footer-text footer-text-visit">Пн-Вс 10:00 - 20:00</p>
+                </div>
+                <div class="footer-info">
+                  <p class="footer-text">Сервисный центр</p>
+                  <a href="Tel:+74993507692" class="footer-phone">+7 (499) 350-76-92</a>
+                  <p class="footer-text footer-text-visit">Пн-Вс 10:00 - 20:00</p>
+                </div>
+              </div>
+          </div>
+        </div>
+        <hr class="footer-seporator">
+        <div class="footer-bottom-application">
+          <div class="apps-left">
+            <a href="./" class="logo-link logo-link-footer">
+              <svg class="social-icon" width="116" height="43">
+                <use href="img/sprite.svg#logo"></use>
+              </svg>
+            </a>
+            <a class="apps-google">
+              <picture>
+                <source type="image/webp" srcset="img/google.webp">
+                <source type="image/jpeg" srcset="img/google.png">
+                <img src="img/google.png" class="apps-image" width="151" height="45">
+              </picture>
+            </a>
+            <a class="apps-google">
+              <picture>
+                <source type="image/webp" srcset="img/apple.webp">
+                <source type="image/jpeg" srcset="img/apple.png">
+                <img src="img/apple.png" class="apps-image" width="135" height="45">
+              </picture>
+            </a>
+          </div>
+          <div class="apps-right">
+                <div class="box box-vk">
+                  <svg class="apps-icon" width="20" height="20">
+                    <use href="img/sprite.svg#vk"></use>
+                  </svg>
+                  <div class="box-content">
+                    <h6 class="box-title">Вконтакте</h6>
+                    <p class="box-text">3 300</p>
+                  </div>
+                </div>
+                <div class="box box-youtube">
+                  <svg class="apps-icon" width="20" height="20">
+                    <use href="img/sprite.svg#yt"></use>
+                  </svg>
+                  <div class="box-content">
+                    <h6 class="box-title">YouTube</h6>
+                    <p class="box-text">3 603</p>
+                  </div>
+                </div>
+                <div class="box box-telegram">
+                  <svg class="apps-icon" width="20" height="20">
+                    <use href="img/sprite.svg#tg"></use>
+                  </svg>
+                  <div class="box-content">
+                    <h6 class="box-title">Telegram</h6>
+                    <p class="box-text">432</p>
+                  </div>
+                </div>
+          </div>
+        </div>
+        <hr class="footer-seporator">
+        <div class="footer-wrapper">
+          <div class="footer-legal">
+            <a href="#" class="props-link">Реквизиты</a>
+            <a href="#" class="footer-policy">
+              Политика конфиденциальности
+            </a>
+          </div>
+          <div class="footer-content-right">
+            <div class="pay-sistem">
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="26" height="11">
+                    <use href="img/sprite.svg#g-pay"></use>
+                  </svg>
+                </div>
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="26" height="11">
+                    <use href="img/sprite.svg#apple-pay"></use>
+                  </svg>
+                </div>
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="25" height="9">
+                    <use href="img/sprite.svg#visa"></use>
+                  </svg>
+                </div>
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="23" height="14">
+                    <use href="img/sprite.svg#mast-card"></use>
+                  </svg>
+                </div>
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="22" height="14">
+                    <use href="img/sprite.svg#maestro"></use>
+                  </svg>
+                </div>
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="24" height="16">
+                    <use href="img/sprite.svg#webmoney"></use>
+                  </svg>
+                </div>
+                <div class="pay-sistem-card">
+                  <svg class="pay-icon" width="24" height="16">
+                    <use href="img/sprite.svg#qiwi"></use>
+                  </svg>
+                </div>
+            </div>
+            <div class="social-media">
+              <p class="social-text">
+                Online чат:
+              </p>
+              <div class="footer-social-link">
+                <a href="#" class="footer-link-social">
+                  <svg class="social-icon" width="12" height="12">
+                    <use href="img/sprite.svg#viber"></use>
+                  </svg>
+                </a>
+                <a href="#" class="footer-link-social">
+                  <svg class="social-icon" width="12" height="12">
+                    <use href="img/sprite.svg#whatsapp"></use>
+                  </svg>
+                </a>
+                <a href="#" class="footer-link-social">
+                  <svg class="social-icon" width="12" height="12">
+                    <use href="img/sprite.svg#telegram"></use>
+                  </svg>
+                </a>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </footer>
 
